3afd0ff247486e7fd73cb142fa80110fe3befb0d
=Andrei <balasca.andrei@gmail.com>
2023-05-08 14:34:11 +0300

    testing again

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,4 +6,6 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const gallery = document.querySelector('.galery')
+
 console.log(galleryItems);
