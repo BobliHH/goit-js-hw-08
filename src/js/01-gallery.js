30363389c3363b304b7f163aa373469e21a65f3b
=Andrei <balasca.andrei@gmail.com>
2023-05-08 15:01:45 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,6 +6,39 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-const gallery = document.querySelector('.galery')
+const gallery = document.querySelector('.galery');
+const galleryItem = creategalleryItems(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryItem);
+
+function creategalleryItems(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class = "galllery__item">
+        <a class = "gallery__link" href = "${original}">
+        <img class = "gallery__image" src = "${preview}" 
+        data-source = "${original}" alt = "${description}"/>
+        </a>
+        </div>`;
+    })
+    .join(' ');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+// const instance = basicLightbox.create(
+//   `
+// <img width="100%" height="100%" src="">`,
+//   {
+//     onShow: (instance) => {
+//       window.addEventListener("keydown", onEscKeyPress);
+//     },
+//     onClose: (instance) => {
+//       window.removeEventListener("keydown", onEscKeyPress);
+//     },
+//   }
+// );
 
 console.log(galleryItems);
