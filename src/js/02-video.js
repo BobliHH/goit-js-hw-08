6935dae21576b3dcf573dd4c182aab908a7d0fc4
=Andrei <balasca.andrei@gmail.com>
2023-05-10 17:37:37 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,13 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const clickPlay = function (time) {
+    localStorage.setItem('videoplayer-current-time', time.seconds);
+}
+
+player.on('timeupdate', throttle(clickPlay, 1000));
+
+
