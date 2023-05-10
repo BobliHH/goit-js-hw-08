c499bca97ad9378a0f12b6edae900cda7cddf642
=Andrei <balasca.andrei@gmail.com>
2023-05-10 18:26:45 +0300

    updated lodash and updated video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,32 @@
 import Player from '@vimeo/player';
-import throttle from 'lodash';
+import throttle from 'lodash/throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const clickPlay = function (time) {
-    localStorage.setItem('videoplayer-current-time', time.seconds);
-}
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
 
-player.on('timeupdate', throttle(clickPlay, 1000));
+player.on('timeupdate', throttle(onPlay, 1000));
 
+const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
 
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+  
