3a364ab79d6bfcdc5ce6d6496fdf22ebd29b5c29
=Andrei <balasca.andrei@gmail.com>
2023-05-12 19:56:50 +0300

    started on exercise 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,13 @@
+import throttle from 'lodash/throttle';
 
+const key = 'feedback-form-state';
+
+const feedbackForm = document.querySelector('feedback-form');
+
+let inputForm = JSON.parse(localStorage.getItem(key));
+const { email, message } = feedbackForm.elements;
+
+function onInput(e) {
+    inputForm = { email: email.value, message: message.value };
+    localStorage.setItem(key, JSON.parse(inputForm));
+}
